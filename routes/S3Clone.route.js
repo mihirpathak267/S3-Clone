9b2eaf05681ada1e8942fe799a9c3dc284ddd8d6
mihir pathak <mihirpathak267@gmail.com>
2023-08-17 16:47:43 +0530

    Add getFiles api end point
    Gets bucktname from request body. Uses that to get all the files in
    that bucket and then sends the array of file names back.

--- routes/S3Clone.route.js
+++ routes/S3Clone.route.js
@@ -21,6 +21,34 @@ router.get('/getBuckets', auth.userAuth, async(req, res)=>{
         }
     })
 })
+
+router.get('/getFiles', auth.userAuth, async(req, res)=>{
+    const bucketName = req.body.bucketName;
+    console.log(req.body);
+    if (!bucketName){
+        return res.json({status: false, message:"Provide bucket name"});
+    }
+    try{
+        const dirPath = path.join(`rootFolder/${bucketName}`);
+        if (!fs.existsSync(dirPath)){
+            return res.json({status: 404, message:"Bucket name is invalid"});
+        }
+        fs.readdir(dirPath, (err, files)=>{
+            const allFiles = files.filter(file=>{
+                const filePath = path.join(dirPath, file);
+                return fs.statSync(filePath).isFile();
+            });
+            if (allFiles.length==0){
+                return res.json({status: true, message:"No files found"});
+            }
+            return res.json({status: true, allFiles: allFiles});
+
+        })
+    } catch(error){
+        console.log(error);
+    }
+})
+
 router.post('/createBucket', auth.userAuth, async(req, res)=>{
     const folderName = req.body.folderName;
     if (!folderName){
@@ -45,6 +73,7 @@ router.post('/createBucket', auth.userAuth, async(req, res)=>{
     }
 
 });
+
 router.post('/upload', auth.userAuth, upload().single('file'), async(req, res)=>{
     if (req.file){
         console.log(req.file);
